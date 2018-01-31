ede2b163d9a2f1378c006f01dd1dcdb8faf50d7d
Trent Patterson <jtrent238@outlook.com>
2018-01-31 11:13:22 -0500

    Update ModRegistry.java

--- src/main/java/com/jtrent238/oredictmaster/ModRegistry.java
+++ src/main/java/com/jtrent238/oredictmaster/ModRegistry.java
@@ -8,7 +8,9 @@ import cpw.mods.fml.common.Loader;
 
 public class ModRegistry {
 
-	private static boolean isjtrent238youtubersLoaded;
+	
+	public static boolean orespawnLoaded;
+	public static boolean nevermineLoaded;
 	
 	public ModRegistry(){
 		
@@ -22,22 +24,22 @@ public class ModRegistry {
 		
 
 		if (Loader.isModLoaded("OreSpawn")) {
-			
 			System.out.println("OreSpawn Mod Loaded");
+			boolean orespawnLoaded = true;
 			try {
-	            LogHelper.log(Level.INFO, "Loaded OreSpawn Mod");
+	            		LogHelper.log(Level.INFO, "Loaded OreSpawn Mod");
 	        }
-	        catch (Exception e) {
-	            LogHelper.log(Level.SEVERE, "Could not load OreSpawn Mod");
-	            e.printStackTrace(System.err);
+	        	catch (Exception e) {
+	         	   LogHelper.log(Level.SEVERE, "Could not load OreSpawn Mod");
+	        	    e.printStackTrace(System.err);
 	        }
 	    
 		if (Loader.isModLoaded("nevermine")) {
-				
 			System.out.println("Advent of Ascension Loaded");
+			boolean nevermineLoaded = true;
 			try {
 		           LogHelper.log(Level.INFO, "Loaded Advent of Ascension");
-		       }
+		  }
 		       catch (Exception e) {
 		           LogHelper.log(Level.SEVERE, "Could not load Advent of Ascension");
 		           e.printStackTrace(System.err);
